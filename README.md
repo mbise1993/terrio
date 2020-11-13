07fe9fff5a65c2905e6b8bf7790c05250e979f66
Matt Bise <mbise1993@gmail.com>
2020-11-12 18:13:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Typescript Starter
+A starter template for bootstrapping a React with Typescript and Parcel bundler.
+
+## Scripts
+* `start` - Start the app in watch mode
+* `build` - Build the app for production
+* `test` - Run tests with Jest
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
